ac0da031050b7107afda15bee011149e8335af23
balkrushnapatel <141103318+balkrushnapatel@users.noreply.github.com>
2023-08-02 08:53:43 +0530

    Add files via upload

---
+++ Lab 207(1)/Program 3.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+	printf("Name     = \"Balkrushna Patel\"\n");
+	printf("Semester = \"Semester number 1\"\n");
+	printf("Roll No. = \"144\"\n");
+	printf("Address  = \"Khodiyar Colony\"\n");
+	return 0;
+}
+
