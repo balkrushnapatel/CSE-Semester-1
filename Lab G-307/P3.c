9ee98e586c7e733f10838e8475409142dcb49496
balkrushnapatel <141103318+balkrushnapatel@users.noreply.github.com>
2023-09-11 08:32:47 +0530

    Add files via upload

---
+++ Lab G-307/P3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main (){
+	
+	float a,b,c;
+	printf("Enter Number ");
+	scanf("%f" ,&a);
+	printf("Enter Number ");
+	scanf("%f" ,&b);
+	printf("Enter Number ");
+	scanf("%f" ,&c);
+	printf("%f" ,(a+b+c)/3);
+	
+}
