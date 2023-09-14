3571466a731ae0b118fe47234e0c1aa917ba3d05
balkrushnapatel <141103318+balkrushnapatel@users.noreply.github.com>
2023-09-14 09:54:36 +0530

    Add files via upload

---
+++ Lab G-307/temperature in fahrenheight to celsius.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void main(){
+	
+ float c,f;
+  printf("enter temperature in fehrenheit");	
+	scanf("%f",&f);
+	
+	c=((f-32)*5)/9;
+	printf("temperature in celsius= %f",c);
+ 	
+	
+}
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
