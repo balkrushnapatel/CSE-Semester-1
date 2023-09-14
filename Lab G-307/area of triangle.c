2b90e0fb6343800b3047eb9b5827d2cf0b4307fc
balkrushnapatel <141103318+balkrushnapatel@users.noreply.github.com>
2023-09-14 09:51:57 +0530

    Add files via upload

---
+++ Lab G-307/area of triangle.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main(){
+	
+	float h,b;
+	printf("enter height: ");
+	scanf("%f",&h);
+	printf("enter base: ");
+	scanf("%f",&b);
+	printf("%f",h*b*1/2);
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
