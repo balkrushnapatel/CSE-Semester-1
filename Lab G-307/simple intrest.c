e8df416bea81650321bed7552f541817909e06c9
balkrushnapatel <141103318+balkrushnapatel@users.noreply.github.com>
2023-09-14 09:53:30 +0530

    Add files via upload

---
+++ Lab G-307/simple intrest.c
@@ -0,0 +1,26 @@
+void main(){
+	
+	float p,t,r;
+	printf("enter principal value");
+	scanf("%f",&p);
+	printf("enter time");
+	scanf("%f",&t);
+	printf("enter rate of intrest");
+	scanf("%f",&r);
+	printf("%f",p*t*r/100);
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
