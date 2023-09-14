2f0149dfe1f4b46e4812511c9e9bb5545047f287
balkrushnapatel <141103318+balkrushnapatel@users.noreply.github.com>
2023-09-14 09:48:39 +0530

    Add files via upload

---
+++ Lab G-307/area of circle.c
@@ -0,0 +1,28 @@
+ #include<stdio.h>
+ void main(){
+ 	float r;
+ 	printf("enter radius");
+ 	scanf("%f",&r);
+ 	printf("%f unit sq.",r*r*3.14);
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
