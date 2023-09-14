740a8c7e7d8c56284510e07916dce3b24890ede0
balkrushnapatel <141103318+balkrushnapatel@users.noreply.github.com>
2023-09-14 09:47:46 +0530

    Add files via upload

---
+++ Lab G-307/average of 3 numbers.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main(){
+	float a,b,c;
+	printf("enter number ");
+	scanf("%f",&a);
+	printf("enter number ");
+	scanf("%f",&b);
+	printf("enter number ");
+	scanf("%f",&c);
+	printf("%f",(a+b+c)/3);
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
